bef9e7b0ce9310baea9c28e545e1890b5823e9cb
Developer <Jaxadev>
2022-02-01 14:51:32 +0500

    Initial commit

---
+++ app/src/main/java/com/example/khalmatov_sarvar_exam/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.khalmatov_sarvar_exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
